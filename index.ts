c2cc4d8c67a2e24990d04e98f601dc9288e4fdf6
Perttu Savolainen <savolainen.perttu@gmail.com>
2020-01-26 16:53:17 +0200

    feat: Add debounce to tag updated event handling
    - Add ruuvitag related interfaces
    - Add postinstall script: allow node process to access ble

--- index.ts
+++ index.ts
@@ -1,7 +1,28 @@
 import ruuvi = require('node-ruuvitag');
+import { debounce } from "debounce";
+
 import { discoverBridge, alertGroupLightState } from "./hue";
 
 require('dotenv').config();
+
+interface IRuuviTag {
+  _events: any;
+  _eventsCount: number;
+  id: string;
+  on: Function;
+}
+
+interface IRuuviTagData {
+  dataFormat: number; 
+  rssi: number; 
+  humidity: number; 
+  temperature: number;
+  pressure: number;
+  accelerationX: number;
+  accelerationY: number;
+  accelerationZ: number;
+  battery: number;
+}
 
 const {
   HUE_USERNAME,
@@ -9,24 +30,25 @@ const {
   TEMPERATURE_THRESHOLD,
 } = process.env;
 
-ruuvi.on('found', (tag) => {
-  const { id } = tag;
-  console.log('Found RuuviTag, id: ' + id, JSON.stringify(tag, null, '\t'));
+const handleRuuviTagData = async (tagId: string, data: IRuuviTagData) => {
+  const { temperature } = data;
+  console.log(`Got data from RuuviTag: ${tagId} ${JSON.stringify(data, null, '\t')}`);
 
-  tag.on('updated', async (data) => {
-    const { temperature } = data;
-    console.log('Got data from RuuviTag ' + tag.id + ':\n' + JSON.stringify(data, null, '\t'));
-    if (temperature > +TEMPERATURE_THRESHOLD) {
-      try {
-        // connect to hue bridge, and update the light(s)
-        const api = await discoverBridge(HUE_USERNAME);
-        await alertGroupLightState(api, +HUE_GROUP_ID);
+  if (temperature > +TEMPERATURE_THRESHOLD) {
+    try {
+      // connect to hue bridge, and update the light(s)
+      const api = await discoverBridge(HUE_USERNAME);
+      await alertGroupLightState(api, +HUE_GROUP_ID);
 
-        // TODO fix it so it won't alarm all the time after threshold had been reached
-        process.exit();
-      } catch (err) { 
-        console.error(err);
-      }    
-    }
-  });
+      // TODO fix it so it won't alarm all the time after threshold had been reached
+      // process.exit(0);
+    } catch (err) { 
+      console.error(err);
+    }    
+  }
+};
+
+ruuvi.on('found', (tag: IRuuviTag) => {
+  console.log('Found tag with id: ' + tag.id);
+  tag.on('updated', debounce((data: IRuuviTagData) => handleRuuviTagData.apply(null, [tag.id, data]), 2000, true));
 });
